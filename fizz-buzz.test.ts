8ba5c7f8cf0f2f30976d79acc4275b4b4733f4d1
Patrick Drechsler <patrick@pdrechsler.de>
2020-04-02 19:46:43 +0200

    some example numbers

--- fizz-buzz.test.ts
+++ fizz-buzz.test.ts
@@ -1,3 +1,8 @@
-test('1 returns 1', () => {
-
+test.each([
+    [1, "1"],
+    [2, "2"],
+    [4, "4"]
+])('%i returns %s', (inputNumber, expected) => {
+    const fizzBuzz = (input: number) => input.toString();
+    expect(fizzBuzz(inputNumber)).toEqual(expected);
 });
