e20c5d7176a1f691fd6fbafb7996f1108a0eadd2
Christoph Welcz <christoph@welcz.de>
2020-04-06 19:17:45 +0200

    made test fail

--- fizz-buzz.test.ts
+++ fizz-buzz.test.ts
@@ -1,8 +1,3 @@
-test.each([
-    [1, "1"],
-    [2, "2"],
-    [4, "4"]
-])('%i returns %s', (inputNumber, expected) => {
-    const fizzBuzz = (input: number) => input.toString();
-    expect(fizzBuzz(inputNumber)).toEqual(expected);
+test('canary', () => {
+  expect(true).toEqual(false);
 });
